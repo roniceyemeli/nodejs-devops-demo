d7d3edc035e31700a19a3405bbd7d80d2a206757
roniceyemeli <roniceyemeli@gmail.com>
2026-02-04 10:55:51 +0100

    feat: launch cd workflow

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/health-check", (req, res) => {
-  res.status(200).send("OK its working");
+  res.status(200).send("OK its working with ci/cd");
 });
 
 // Error handling middleware
